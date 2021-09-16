9bbd3d11ad87fef4585d62092ca31e1947bb747f
Mirenk <minikmwinsea@gmail.com>
2021-09-16 17:12:34 +0900

    Protobuf: Packaging

--- semibot_host/matchingapis/build.sh
+++ semibot_host/matchingapis/build.sh
@@ -4,7 +4,9 @@ cd `dirname $0`
 
 mkdir -p out
 
-for file in `ls -1 matching/*.proto`
+for file in `ls -1 matching_pb/*.proto`
 do
   python3 -m grpc_tools.protoc -I./ -I ./vendor/googleapis/ --python_out=./out --grpc_python_out=./out ./"$file"
 done
+
+touch out/matching_pb/__init__.py

cbf12ca0a4797dbcc6858cb1e22b6ef1afad763a
Mirenk <minikmwinsea@gmail.com>
2021-09-17 13:13:32 +0900

    Protobuf: Stop making __init__.py

--- semibot_host/matchingapis/build.sh
+++ semibot_host/matchingapis/build.sh
@@ -8,5 +8,3 @@ for file in `ls -1 matching_pb/*.proto`
 do
   python3 -m grpc_tools.protoc -I./ -I ./vendor/googleapis/ --python_out=./out --grpc_python_out=./out ./"$file"
 done
-
-touch out/matching_pb/__init__.py

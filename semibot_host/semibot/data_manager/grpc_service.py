5caf8cc4f391ffd987efa507b214a055f6190b89
Mirenk <minikmwinsea@gmail.com>
2021-09-19 19:25:39 +0900

    data_manager: Impl ListPersonalData

--- semibot_host/semibot/data_manager/grpc_service.py
+++ semibot_host/semibot/data_manager/grpc_service.py
@@ -24,7 +24,26 @@ class DataManage(data_manage_pb2_grpc.DataManageServicer):
         return response
 
     def ListPersonalData(self, request, context):
-        pass
+        response = data_manage_pb2.ListPersonalDataResponse()
+        personal_data_list = PersonalData.objects.all()
+
+        for personal_data in personal_data_list:
+            # ラベル以外のPersonalDataを先に作成
+            personal_data_pb = type_pb2.PersonalData()
+            personal_data_pb.id = personal_data.username
+            personal_data_pb.name = personal_data.first_name + personal_data.last_name
+            personal_data_pb.message_addr = personal_data.message_addr
+
+            # ProtobufのLabelリストを生成
+            label_list = self.__get_label_pb_list_from_label_queryset(personal_data.label.all())
+
+            # ラベルを登録
+            personal_data_pb.labels.extend(label_list)
+
+            # responseに追加
+            response.personal_data.append(personal_data_pb)
+
+        return response
 
     def ListTasks(self, request, context):
         pass
--- semibot_host/semibot/data_manager/test_grpc.py
+++ semibot_host/semibot/data_manager/test_grpc.py
@@ -1,7 +1,7 @@
 import grpc
 from django_grpc_framework.test import RPCTestCase
 from .matching_pb import data_manage_pb2, data_manage_pb2_grpc
-from data_manager.models import Label
+from data_manager.models import Label, PersonalData
 
 class DataManageTest(RPCTestCase):
     def test_list_labels(self):
@@ -13,3 +13,22 @@ class DataManageTest(RPCTestCase):
         self.assertEqual(res.labels[0].name, 'test1')
         self.assertEqual(res.labels[1].name, 'test2')
         self.assertEqual(len(res.labels), 2)
+
+    def test_list_personal_data(self):
+        label1 = Label.objects.create(name='test1')
+        label2 = Label.objects.create(name='test2')
+
+        testperson = PersonalData.objects.create(username='testuser', first_name='test', last_name='user', message_addr='http://example.com')
+
+        testperson.label.add(label1)
+        testperson.label.add(label2)
+
+        stub = data_manage_pb2_grpc.DataManageStub(self.channel)
+        res = stub.ListPersonalData(data_manage_pb2.ListPersonalDataRequest())
+
+        print(res)
+        self.assertEqual(res.personal_data[0].id, testperson.username)
+        self.assertEqual(res.personal_data[0].name, testperson.first_name + testperson.last_name)
+        self.assertEqual(res.personal_data[0].message_addr, testperson.message_addr)
+        self.assertEqual(res.personal_data[0].labels[0].name, 'test1')
+        self.assertEqual(res.personal_data[0].labels[1].name, 'test2')

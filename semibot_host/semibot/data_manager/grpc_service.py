20733f41c7f14eeaf1cfc08fcb750eb2d5d495f7
Mirenk <minikmwinsea@gmail.com>
2021-09-19 18:26:25 +0900

    data_manager: gRPC: Add 'Label QuerySet -> Protobuf List' function

--- semibot_host/semibot/data_manager/grpc_service.py
+++ semibot_host/semibot/data_manager/grpc_service.py
@@ -4,13 +4,22 @@ from .matching_pb import data_manage_pb2, data_manage_pb2_grpc, type_pb2
 
 # サービス定義
 class DataManage(data_manage_pb2_grpc.DataManageServicer):
-    def ListLabels(self, request, context):
-        response = data_manage_pb2.ListLabelsResponse()
-        labels = Label.objects.all()
+    # ラベルのqueryset -> ProtobufのLabelリスト
+    @classmethod
+    def __get_label_pb_list_from_label_queryset(cls, labels):
+        labels_pb = []
 
         for label in labels:
             label_pb = type_pb2.Label(name=label.name)
-            response.labels.append(label_pb)
+            labels_pb.append(label_pb)
+
+        return labels_pb
+
+    def ListLabels(self, request, context):
+        response = data_manage_pb2.ListLabelsResponse()
+        labels_pb = self.__get_label_pb_list_from_label_queryset(Label.objects.all())
+
+        response.labels.extend(labels_pb)
 
         return response
 

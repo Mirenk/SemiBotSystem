f75a138cd092ec73b3ace7f44bf4c93160fddee0
Mirenk <minikmwinsea@gmail.com>
2021-09-20 17:52:33 +0900

    Protobuf: candidates -> worker

--- semibot_host/semibot/data_manager/matching_pb/type_pb2.py
+++ semibot_host/semibot/data_manager/matching_pb/type_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\ntype.proto\x12\x08matching\x1a\x1fgoogle/protobuf/timestamp.proto\"\x15\n\x05Label\x12\x0c\n\x04name\x18\x01 \x01(\t\";\n\nLabelValue\x12\x1e\n\x05label\x18\x01 \x01(\x0b\x32\x0f.matching.Label\x12\r\n\x05value\x18\x02 \x01(\x05\"_\n\x0cPersonalData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x14\n\x0cmessage_addr\x18\x03 \x01(\t\x12\x1f\n\x06labels\x18\x04 \x03(\x0b\x32\x0f.matching.Label\"o\n\x04Task\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\rrequire_label\x18\x02 \x03(\x0b\x32\x0f.matching.Label\x12\x31\n\x13require_label_value\x18\x03 \x03(\x0b\x32\x14.matching.LabelValue\"\xff\x01\n\x0fTaskRequestData\x12\x14\n\x0crequest_name\x18\x01 \x01(\t\x12\x1c\n\x04task\x18\x02 \x01(\x0b\x32\x0e.matching.Task\x12-\n\ttask_date\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12*\n\ncandidates\x18\x04 \x03(\x0b\x32\x16.matching.PersonalData\x12(\n\x0frecommend_label\x18\x05 \x03(\x0b\x32\x0f.matching.Label\x12\x33\n\x15recommend_label_value\x18\x06 \x03(\x0b\x32\x14.matching.LabelValueb\x06proto3'
+  serialized_pb=b'\n\ntype.proto\x12\x08matching\x1a\x1fgoogle/protobuf/timestamp.proto\"\x15\n\x05Label\x12\x0c\n\x04name\x18\x01 \x01(\t\";\n\nLabelValue\x12\x1e\n\x05label\x18\x01 \x01(\x0b\x32\x0f.matching.Label\x12\r\n\x05value\x18\x02 \x01(\x05\"_\n\x0cPersonalData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x14\n\x0cmessage_addr\x18\x03 \x01(\t\x12\x1f\n\x06labels\x18\x04 \x03(\x0b\x32\x0f.matching.Label\"o\n\x04Task\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\rrequire_label\x18\x02 \x03(\x0b\x32\x0f.matching.Label\x12\x31\n\x13require_label_value\x18\x03 \x03(\x0b\x32\x14.matching.LabelValue\"\xfb\x01\n\x0fTaskRequestData\x12\x14\n\x0crequest_name\x18\x01 \x01(\t\x12\x1c\n\x04task\x18\x02 \x01(\x0b\x32\x0e.matching.Task\x12-\n\ttask_date\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12&\n\x06worker\x18\x04 \x03(\x0b\x32\x16.matching.PersonalData\x12(\n\x0frecommend_label\x18\x05 \x03(\x0b\x32\x0f.matching.Label\x12\x33\n\x15recommend_label_value\x18\x06 \x03(\x0b\x32\x14.matching.LabelValueb\x06proto3'
   ,
   dependencies=[google_dot_protobuf_dot_timestamp__pb2.DESCRIPTOR,])
 
@@ -227,7 +227,7 @@ _TASKREQUESTDATA = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='candidates', full_name='matching.TaskRequestData.candidates', index=3,
+      name='worker', full_name='matching.TaskRequestData.worker', index=3,
       number=4, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -260,7 +260,7 @@ _TASKREQUESTDATA = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=352,
-  serialized_end=607,
+  serialized_end=603,
 )
 
 _LABELVALUE.fields_by_name['label'].message_type = _LABEL
@@ -269,7 +269,7 @@ _TASK.fields_by_name['require_label'].message_type = _LABEL
 _TASK.fields_by_name['require_label_value'].message_type = _LABELVALUE
 _TASKREQUESTDATA.fields_by_name['task'].message_type = _TASK
 _TASKREQUESTDATA.fields_by_name['task_date'].message_type = google_dot_protobuf_dot_timestamp__pb2._TIMESTAMP
-_TASKREQUESTDATA.fields_by_name['candidates'].message_type = _PERSONALDATA
+_TASKREQUESTDATA.fields_by_name['worker'].message_type = _PERSONALDATA
 _TASKREQUESTDATA.fields_by_name['recommend_label'].message_type = _LABEL
 _TASKREQUESTDATA.fields_by_name['recommend_label_value'].message_type = _LABELVALUE
 DESCRIPTOR.message_types_by_name['Label'] = _LABEL

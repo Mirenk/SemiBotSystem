fc77d71225db768289b63177f6dad361113794fa
Mirenk <minikmwinsea@gmail.com>
2021-09-21 15:12:06 +0900

    matching: Impl Models

--- semibot_host/semibot/matching/models.py
+++ semibot_host/semibot/matching/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+# ラベル
+class Label(models.Model):
+    name = models.CharField(unique=True, max_length=20)
+
+# 数値ラベル
+class LabelValue(models.Model):
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    value = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['label', 'value'], name='labelvalue_unique')
+        ]
+
+# ラベルセット
+# 固定ラベルと数値ラベルのセットを保存しておくモデル
+class LabelSet(models.Model):
+    const_label = models.ManyToManyField(Label, blank=True)
+    var_label = models.ManyToManyField(LabelValue, blank=True)
+
+# 依頼リクエスト
+# TODO: 重複登録の防止、ほぼ同一のような依頼リクエストが来ることも考えられるため、それも考慮
+class TaskRequestRequest(models.Model):
+    name = models.CharField(max_length=20)
+    task = models.CharField(max_length=20)
+    task_datetime = models.DateTimeField()
+    label_set = models.ManyToManyField(LabelSet)
+    matching_end_datetime = models.DateTimeField()
+    callback_url = models.URLField()
+

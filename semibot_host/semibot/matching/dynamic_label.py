c473e9267dcf827654710aaecc60859feb594556
Mirenk <minikmwinsea@gmail.com>
2021-09-26 18:48:27 +0900

    matching: Add type hint to DynamicLabel functions

--- semibot_host/semibot/matching/dynamic_label.py
+++ semibot_host/semibot/matching/dynamic_label.py
@@ -1,5 +1,17 @@
 # 履歴から生成されるラベル(動的ラベル)
+# クラスメソッドで動作を書いていく
+from typing import List
+from .matching_pb import type_pb2
 
 class DynamicLabel:
     @classmethod
-    def 
+    def recent_joined(cls,
+                      personal_data: List[type_pb2.PersonalData],
+                      task_request_history: List[type_pb2.TaskRequestData]):
+        pass
+
+    @classmethod
+    def past_joined(cls,
+                    personal_data: List[type_pb2.PersonalData],
+                    task_request_history: List[type_pb2.TaskRequestData]):
+        pass

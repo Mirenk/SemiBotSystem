333b840fbbb1bb8a6c215d38b898d25f08f542af
Mirenk <minikmwinsea@gmail.com>
2021-10-08 19:38:39 +0900

    Protobuf: Fix build

--- semibot_host/matchingapis/build.sh
+++ semibot_host/matchingapis/build.sh
@@ -9,7 +9,11 @@ echo "import sys" > matching_pb/matching_pb/__init__.py
 echo "from pathlib import Path" >> matching_pb/matching_pb/__init__.py
 echo "sys.path.append(str(Path(__file__).parent))" >> matching_pb/matching_pb/__init__.py
 
-for file in `ls -1 matching/*.proto`
+cp matching/* matching_pb/
+
+for file in `ls -1 matching_pb/*.proto`
 do
-  python3 -m grpc_tools.protoc -I./matching -I ./vendor/googleapis/ --python_out=./matching_pb/matching_pb --grpc_python_out=./matching_pb/matching_pb ./"$file"
+  python3 -m grpc_tools.protoc -I./ -I ./vendor/googleapis/ --python_out=./matching_pb/ --grpc_python_out=./matching_pb/ ./"$file"
 done
+
+rm -rf matching_pb/*.proto

df7dd17049bcbb38266787b67b9336c4b4b33ed3
Mirenk <minikmwinsea@gmail.com>
2021-10-08 18:13:23 +0900

    Protobuf: Install package

--- semibot_host/matchingapis/build.sh
+++ semibot_host/matchingapis/build.sh
@@ -2,10 +2,14 @@
 
 cd `dirname $0`
 
-rm -rf matching_pb
-mkdir -p matching_pb
+rm -rf matching_pb/matching_pb
+mkdir -p matching_pb/matching_pb
+
+echo "import sys" > matching_pb/matching_pb/__init__.py
+echo "from pathlib import Path" >> matching_pb/matching_pb/__init__.py
+echo "sys.path.append(str(Path(__file__).parent))" >> matching_pb/matching_pb/__init__.py
 
 for file in `ls -1 matching/*.proto`
 do
-  python3 -m grpc_tools.protoc -I./matching -I ./vendor/googleapis/ --python_out=./matching_pb --grpc_python_out=./matching_pb ./"$file"
+  python3 -m grpc_tools.protoc -I./matching -I ./vendor/googleapis/ --python_out=./matching_pb/matching_pb --grpc_python_out=./matching_pb/matching_pb ./"$file"
 done

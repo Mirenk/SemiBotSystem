37e2bde3b5344f5eaa7602c092253e4e9158073f
Mirenk <minikmwinsea@gmail.com>
2021-10-08 20:33:22 +0900

    Protobuf: Remove unused code from __init__.py

--- semibot_host/matchingapis/build.sh
+++ semibot_host/matchingapis/build.sh
@@ -5,9 +5,7 @@ cd `dirname $0`
 rm -rf matching_pb/matching_pb
 mkdir -p matching_pb/matching_pb
 
-echo "import sys" > matching_pb/matching_pb/__init__.py
-echo "from pathlib import Path" >> matching_pb/matching_pb/__init__.py
-echo "sys.path.append(str(Path(__file__).parent))" >> matching_pb/matching_pb/__init__.py
+touch matching_pb/matching_pb/__init__.py
 
 cp matching/* matching_pb/
 

8236226ac4e6cac93b45a3517e685fca3a7c6c16
Mirenk <minikmwinsea@gmail.com>
2021-10-10 18:53:07 +0900

    matching: Fix gRPC client

--- semibot_host/semibot/matching/grpc_client.py
+++ semibot_host/semibot/matching/grpc_client.py
@@ -25,7 +25,7 @@ def get_personal_data_dict():
     return res
 
 # 作業辞書取得
-def get_task_dict():
+def get_task_list():
     with grpc.insecure_channel(settings.MATCHING_DATAMANAGE_HOST) as channel:
         stub = data_manage_pb2_grpc.DataManageStub(channel)
         res = stub.ListTasks(data_manage_pb2.ListTasksRequest())
@@ -34,9 +34,11 @@ def get_task_dict():
 
 # 上記関数を使い、名前のtype_pb2.Taskオブジェクトを返す関数
 def get_task_from_name(name: str):
-    task_dict = get_task_dict()
+    with grpc.insecure_channel(settings.MATCHING_DATAMANAGE_HOST) as channel:
+        stub = data_manage_pb2_grpc.DataManageStub(channel)
+        res = stub.GetTaskFromName(data_manage_pb2.GetTaskFromNameRequest(name=name))
 
-    return task_dict[name]
+    return res
 
 # 作業オブジェクトに対応した作業履歴を取得
 def get_task_request_histories(task: type_pb2.Task):
--- semibot_host/semibot/matching/tests/test_grpc_client.py
+++ semibot_host/semibot/matching/tests/test_grpc_client.py
@@ -11,3 +11,10 @@ class RPCTest(TestCase):
         personal_data = client.get_personal_data_dict()
 
         print(personal_data)
+
+    def test_get_task_request_histories(self):
+        task = client.get_task_from_name('全体ゼミ')
+
+        task_request_history = client.get_task_request_histories(task)
+
+        print(task_request_history)

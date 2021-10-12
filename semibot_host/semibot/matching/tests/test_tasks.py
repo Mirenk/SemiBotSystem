d0b49e4c0c882aacfcdb4cab14d5bfd7d2dc67cc
Mirenk <minikmwinsea@gmail.com>
2021-10-12 15:22:18 +0900

    matching: Task: Remove check_time test

--- semibot_host/semibot/matching/tests/test_tasks.py
+++ semibot_host/semibot/matching/tests/test_tasks.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from .make_test_data import *
 from datetime import datetime
-from matching.tasks import check_joined_candidates, check_time
+from matching.tasks import check_joined_candidates
 
 class TaskTest(TestCase):
     def test_check_joined_candidates(self):
@@ -10,18 +10,3 @@ class TaskTest(TestCase):
         task_request = make_test_task_request()
 
         check_joined_candidates(task_request.id)
-
-    def test_check_time(self):
-        # 依頼データ生成
-        task_request = make_test_task_request()
-
-        task_request.next_rematching = datetime.now()
-        task_request.save()
-
-        check_time()
-
-# 書き込みが発生するため無効にしてます
-#        task_request.matching_end_datetime = datetime.now()
-#        task_request.save()
-
-#        check_time()

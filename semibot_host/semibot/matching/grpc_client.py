f34cb22b55b443e7c3be383b579f4338a358d08e
Mirenk <minikmwinsea@gmail.com>
2021-10-18 11:09:27 +0900

    matching: gRPC Fix

--- semibot_host/semibot/matching/grpc_client.py
+++ semibot_host/semibot/matching/grpc_client.py
@@ -62,7 +62,7 @@ def record_task_request_history(task_request: TaskRequestRequest):
     task_request_pb.task_date.CopyFrom(timestamp_pb2.Timestamp(seconds=int(task_request.task_datetime.timestamp())))
 
     for candidate in task_request.joined_candidates.all():
-        userid = candidate.personal_data.userid
+        userid = candidate.personal_data.username
         task_request_pb.worker[userid].CopyFrom(personal_data[userid])
 
     # TODO: 現時点では収集に使ったラベルを使用しないため、記録していない
